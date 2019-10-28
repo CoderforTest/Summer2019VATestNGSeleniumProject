f7cc23d148bf334209187f529c3be12edcadb925
CoderforTest <cyclist582@gmail.com>
2019-10-28 13:07:17 -0400

    First commit of Summer2019VATestNGSeleniumProject

---
+++ src/test/java/com/cybertek/tests/Example.java
@@ -0,0 +1,13 @@
+package com.cybertek.tests;
+
+public class Example {
+
+    public static void main(String[] args) {
+
+        String myString = "hello";
+
+        System.out.println(myString);
+
+        System.out.println(myString.toUpperCase());
+    }
+}
---
+++ src/test/java/com/cybertek/tests/vyTrackLogin/Login.java
@@ -0,0 +1,62 @@
+package com.cybertek.tests.vyTrackLogin;
+
+import com.cybertek.utilities.WebDriverFactory;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Login {
+    public static void main(String[] args) throws InterruptedException {
+
+/*
+Go to the login page of VyTrack
+• Enter valid credential (can be any role)
+• Click login button
+• Verify that the user login successfully
+
+By.name(“_username”)
+By.name(“_password”)
+By.id(“_submit”)
+     */
+
+        // calling getDriver method from WebDriverFactory file of utilities package.
+        WebDriver driver = WebDriverFactory.getDriver("chrome");
+
+        // go to https://qa2.vytrack.com/user/login
+        driver.get("https://qa2.vytrack.com/user/login");
+
+        // find Username box
+        WebElement userNameInput = driver.findElement(By.name("_username"));
+
+        // enter Username
+        String expectedUserName = "user19";
+        userNameInput.sendKeys(expectedUserName);
+
+        // find Password box
+        WebElement passwordInput = driver.findElement(By.name("_password"));
+
+        // enter Password
+        String expectedPassword = "UserUser123";
+        passwordInput.sendKeys(expectedPassword);
+
+        // press click button
+        WebElement enterCredentials = driver.findElement(By.id("_submit"));
+        enterCredentials.click();
+
+   String expectedURLResult = "https://qa2.vytrack.com/";
+   String actualURLResult = driver.getCurrentUrl();
+
+        if (expectedURLResult.equals(actualURLResult)) {
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL");
+        }
+
+        // quit from chrome browser
+        driver.quit();
+
+    }
+}
